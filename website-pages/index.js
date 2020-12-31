7c31ef3423abcbe7fcf20b5008589dfc063debf6
MarwaMohamed <marwam.2@hotmail.com>
2020-12-31 00:33:11 -0600

    searching functionality of the search element using js and bootstrap

--- website-pages/index.js
+++ website-pages/index.js
@@ -1,4 +1,6 @@
-/*global document, setInterval*/
+/*global document, setInterval, console*/
+
+/* Start the introduction automatic background image change*/
 var introduction = document.getElementsByClassName("introduction"),
     backgroundImages = ["../images/chanel.jpg", "../images/gucci.jpg", "../images/adidas.jpg", "../images/ralphlauren.jpg", "../images/nike.jpg"],
     index = 1;
@@ -16,3 +18,34 @@ var introduction = document.getElementsByClassName("introduction"),
     }, 2000);
     
 }());
+
+/* End the introduction automatic background image change*/
+
+
+/* Start the Search Filter*/
+var search = "", text = "";
+function filter(e) {
+    "use strict";
+    document.getElementsByClassName('searchSection')[0].style.display = 'block';
+    search = e.value.toLowerCase();
+    console.log(e.value);
+    document.querySelectorAll('.contsearch').forEach(function (row) {
+        text = row.innerText.toLowerCase();
+        if (text.match(search)) {
+            row.style.display = "block";
+        } else {
+            row.style.display = "none";
+        }
+    });
+}
+
+function hideList() {
+    "use strict";
+    document.getElementsByClassName('contsearch').style.display = 'none';
+}
+
+function finishedSearching() {
+     "use strict";
+      document.getElementsByClassName('searchSection')[0].style.display = 'none';
+}
+/* End the Search Filter*/
